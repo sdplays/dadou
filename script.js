9a73a9bccdc4d992f0a7e384fed4008d9d5a5fd3
sdblock <77467572+sdplays@users.noreply.github.com>
2021-11-18 22:38:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ var padding = {top:20, right:40, bottom:0, left:0},
             //randomNumbers = getRandomNumbers();
         //http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
         var data = [
-                    {"label":"Dell LAPTOP",  "value":1,  "question":"What CSS property is used for specifying the area between the content and its border?"}, // padding
-                    {"label":"IMAC PRO",  "value":2,  "question":"What CSS property is used for changing the font?"}, //font-family
-                    {"label":"SUZUKI",  "value":3,  "question":"What CSS property is used for changing the color of text?"}, //color
-                    {"label":"HONDA",  "value":4,  "question":"What CSS property is used for changing the boldness of text?"}, //font-weight
-                    {"label":"FERRARI",  "value":5,  "question":"What CSS property is used for changing the size of text?"}, //font-size
-                    {"label":"APARTMENT",  "value":6,  "question":"What CSS property is used for changing the background color of a box?"}, //background-color
-                    {"label":"IPAD PRO",  "value":7,  "question":"Which word is used for specifying an HTML tag that is inside another tag?"}, //nesting
-                    {"label":"LAND",  "value":8,  "question":"Which side of the box is the third number in: margin:1px 1px 1px 1px; ?"}, //bottom
-                    {"label":"MOTOROLLA",  "value":9,  "question":"What are the fonts that don't have serifs at the ends of letters called?"}, //sans-serif
-                    {"label":"BMW", "value":10, "question":"With CSS selectors, what character prefix should one use to specify a class?"}
+                    {"label":"Dell LAPTOP",  "value":1,  "question":"iktib haja ya hadil 1"}, // padding
+                    {"label":"IMAC PRO",  "value":2,  "question":"iktib do 2"}, //font-family
+                    {"label":"SUZUKI",  "value":3,  "question":"iktib dado 3"}, //color
+                    {"label":"HONDA",  "value":4,  "question":"mizel na9es"}, //font-weight
+                    {"label":"FERRARI",  "value":5,  "question":"5ater kek mich sahil"}, //font-size
+                    {"label":"APARTMENT",  "value":6,  "question":"booo "}, //background-color
+                    {"label":"IPAD PRO",  "value":7,  "question":"lol"}, //nesting
+                    {"label":"LAND",  "value":8,  "question":"yq textoo"}, //bottom
+                    {"label":"MOTOROLLA",  "value":9,  "question":"hjg"}, //sans-serif
+                    {"label":"BMW", "value":10, "question":"dadou 2315"}
         ];
         var svg = d3.select('#chart')
             .append("svg")
@@ -96,6 +96,7 @@ var padding = {top:20, right:40, bottom:0, left:0},
               
                     /* Get the result value from object "data" */
                     console.log(data[picked].value)
+                    alert(data[picked].question)
               
                     /* Comment the below line for restrict spin to sngle time */
                     container.on("click", spin);
